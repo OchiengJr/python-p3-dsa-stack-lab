3f94b0eecddb5305e73a29d5ded3d25524c8c657
hp <chrispine.ochieng@student.moringaschool.com>
2024-05-13 17:04:30 +0300

    commit

--- lib/testing/conftest.py
+++ lib/testing/conftest.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+import pytest
 
 def pytest_itemcollected(item):
-    par = item.parent.obj
+    parent = item.parent.obj
     node = item.obj
-    pref = par.__doc__.strip() if par.__doc__ else par.__class__.__name__
-    suf = node.__doc__.strip() if node.__doc__ else node.__name__
-    if pref or suf:
-        item._nodeid = ' '.join((pref, suf))
+    parent_desc = parent.__doc__.strip() if parent.__doc__ else parent.__class__.__name__
+    node_desc = node.__doc__.strip() if node.__doc__ else node.__name__
+    if parent_desc or node_desc:
+        item._nodeid = ' '.join((parent_desc, node_desc))
+
+if __name__ == "__main__":
+    pytest.main()
